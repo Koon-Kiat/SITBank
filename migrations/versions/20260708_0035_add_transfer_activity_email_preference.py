e51cf990fd9afbf9001bc70738ae6a84e88f7a4a
Ernest Goh <gohzae2002@gmail.com>
2026-07-08 21:34:08 +0800

    Fix duplicate revision ID

--- migrations/versions/20260708_0035_add_transfer_activity_email_preference.py
+++ migrations/versions/20260708_0035_add_transfer_activity_email_preference.py
@@ -1,16 +1,16 @@
 """Add transfer activity email preference.
 
-Revision ID: 20260708_0034
-Revises: 20260708_0033
-Create Date: 2026-07-08 00:34:00
+Revision ID: 20260708_0035
+Revises: 20260708_0034
+Create Date: 2026-07-08 00:35:00
 """
 
 from alembic import op
 import sqlalchemy as sa
 
 
-revision = "20260708_0034"
-down_revision = "20260708_0033"
+revision = "20260708_0035"
+down_revision = "20260708_0034"
 branch_labels = None
 depends_on = None
 
